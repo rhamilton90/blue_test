dca07591cca92ac24cb8b9c7f84d32a0624e048c
sickm3 <reggieh2012@gmail.com>
2016-10-12 23:32:57 -0500

    Initial Commit

---
+++ blue_test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  blue_test
+//
+//  Created by HamiltonMac on 10/12/16.
+//  Copyright © 2016 HamiltonMac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
